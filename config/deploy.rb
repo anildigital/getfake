ef1cc30715f24f55ead732e609f357268519b982
Anil Wadghule <anildigital@gmail.com>
2008-01-29 22:22:26 +0530

    Added capistrano related files

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+set :application, "getfake.icanhasruby.com"
+set :repository,  "anil@icanhasruby.com:/var/git/getfake"
+
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/anil/public_html/#{application}"
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
+
+# ssh port
+set :port, 2222
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+
+ssh_options[:paranoid] = false
+
+set :user, 'anil'
+set :runner, user
+set :use_sudo, 'false'
+
+role :app, application
+role :web, application
+role :db,  application, :primary => true
